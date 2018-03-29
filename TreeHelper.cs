6f5354274df71c4086ced9fe30661b2aa1528dc1
Roger Leblanc <roger@rogerleblanc.net>
2018-03-29 15:48:11 -0400

    Create TreeHelper.cs

---
+++ TreeHelper.cs
@@ -0,0 +1,75 @@
+using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApplication18
+{
+    public static class TreeHelper
+    {
+        public static T FindChildByName<T>(FrameworkElement element, string sChildName) where T : FrameworkElement
+        {
+            T childElement = null;
+            var nChildCount = VisualTreeHelper.GetChildrenCount(element);
+            for (int i = 0; i < nChildCount; i++)
+            {
+                FrameworkElement child = VisualTreeHelper.GetChild(element, i) as FrameworkElement;
+
+                if (child == null)
+                    continue;
+
+                if (child is T && child.Name.Equals(sChildName))
+                {
+                    childElement = (T)child;
+                    break;
+                }
+
+                childElement = FindChildByName<T>(child, sChildName);
+
+                if (childElement != null)
+                    break;
+            }
+            return childElement;
+        }
+
+        public static T FindChild<T>(FrameworkElement element) where T : FrameworkElement
+        {
+            T childElement = null;
+            var nChildCount = VisualTreeHelper.GetChildrenCount(element);
+            for (int i = 0; i < nChildCount; i++)
+            {
+                FrameworkElement child = VisualTreeHelper.GetChild(element, i) as FrameworkElement;
+
+                if (child == null)
+                    continue;
+
+                if (child is T)
+                {
+                    childElement = (T)child;
+                    break;
+                }
+
+                childElement = FindChild<T>(child);
+
+                if (childElement != null)
+                    break;
+            }
+            return childElement;
+        }
+
+        public static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            //get parent item
+            DependencyObject parentObject = VisualTreeHelper.GetParent(child);
+
+            //we've reached the end of the tree
+            if (parentObject == null)
+                return null;
+
+            //check if the parent matches the type we're looking for
+            T parent = parentObject as T;
+            if (parent != null)
+                return parent;
+            else
+                return FindParent<T>(parentObject);
+        }
+    }
+}
